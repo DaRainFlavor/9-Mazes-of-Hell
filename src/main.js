20ed2053c059f24ec57998cb52f4ec5e98718840
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-03-27 20:35:12 +0800

    [Style] [Jermel] Added styles to game over screen.

--- src/main.js
+++ src/main.js
@@ -1,38 +1,37 @@
-import { Boot } from './scenes/Boot';
-import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
-import { Story } from './scenes/Story'; // Import Story Scene
-import { Success } from './scenes/Success';
-
+import { Boot } from "./scenes/Boot";
+import { Game } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import { Preloader } from "./scenes/Preloader";
+import { Story } from "./scenes/Story"; // Import Story Scene
+import { Success } from "./scenes/Success";
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1216,
-    height: 800,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
+  type: Phaser.AUTO,
+  width: 1216,
+  height: 800,
+  parent: "game-container",
+  backgroundColor: "#028af8",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
     },
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        Story,      // Add Story Scene here (right after Preloader)
-        MainMenu,
-        Game,
-        Success,
-        GameOver
-    ]
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [
+    Boot,
+    Preloader,
+    Story, // Add Story Scene here (right after Preloader)
+    MainMenu,
+    Game,
+    Success,
+    GameOver,
+  ],
 };
 
 export default new Phaser.Game(config);
--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -1,28 +1,66 @@
-import { Scene } from 'phaser';
+import { Scene, Textures } from "phaser";
+import WebFontFile from "../scripts/fontLoader";
 
-export class GameOver extends Scene
-{
-    constructor ()
-    {
-        super('GameOver');
+export class GameOver extends Scene {
+  constructor() {
+    super("GameOver");
+  }
+
+  preload() {
+    this.load.addFile(new WebFontFile(this.load, "Nosifer"));
+    this.load.audio("gameOverMusic", "assets/music/Game Over.mp3");
+    this.load.audio("scream", "assets/sfx/Scream1.mp3");
+    this.load.image("gameOverBG", "assets/gameOverBG.jpg");
+  }
+
+  create() {
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+
+    // Background image
+    const bg = this.add.image(0, 0, "gameOverBG").setOrigin(0, 0);
+    bg.setDisplaySize(width, height);
+
+    // Music
+    this.sound.pauseOnBlur = false;
+    this.music = this.sound.add("gameOverMusic", { loop: true, volume: 0.75 });
+    this.music.play();
+
+    // Scream SFX
+    this.music = this.sound.add("scream", { loop: false, volume: 1 });
+    this.music.play();
+
+    // Return to Main Menu button
+    const returnToMenuButton = this.add
+      .text(this.scale.width / 2, 460, "Return to Main Menu", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5, -2);
+    returnToMenuButton.setInteractive();
+    returnToMenuButton.on("pointerdown", () => {
+      this.music.stop(); // Stop music when switching scenes
+      this.scene.start("MainMenu");
+    });
+
+    this.cameras.main.setBackgroundColor("#000000");
+    this.add
+      .text(512, 384, "YOU HAVE PERISHED", {
+        fontFamily: "Nosifer",
+        fontSize: 80,
+        color: "#bf0b0b",
+        align: "center",
+      })
+      .setOrigin(0.41, 2);
+  }
+
+  shutdown() {
+    if (this.music) {
+      this.music.stop();
     }
-
-    create ()
-    {
-        this.cameras.main.setBackgroundColor(0xff0000);
-
-    /*    this.add.image(512, 384, 'background').setAlpha(0.5);*/
-
-        this.add.text(512, 384, 'Game Over Screen', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('MainMenu');
-
-        });
-    }
+  }
 }
